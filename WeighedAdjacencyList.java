2d7ccac5556b5c9ed7a3baac76779001cebb0498
bhargavi101 <32438432+bhargavi101@users.noreply.github.com>
2018-01-02 18:05:57 -0600

    Dijkstra

---
+++ WeighedAdjacencyList.java
@@ -0,0 +1,144 @@
+package Project_2;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+// For file reading
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+
+public class WeighedAdjacencyList<WeighedgraphEdge> {
+    private HashMap<Integer, ArrayList<WeighedgraphEdge>> adj = new HashMap(); // adjacency-list
+
+    public WeighedAdjacencyList() {}
+
+    /**
+     * Instantiate graph from file with data
+     * @param file
+     * @throws IOException
+     */
+    public WeighedAdjacencyList(String file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split("\\s");
+
+            if (parts.length == 3) {
+                int from = Integer.parseInt(parts[0]);
+                int to = Integer.parseInt(parts[1]);
+                double weight = Double.parseDouble(parts[2]);
+
+               // addEdge(new WeighedgraphEdge(from, to, weight));
+            }
+        }
+    }
+
+    /**
+     * @param vertex
+     * @return list of edges vertex is connected to.
+     */
+    public ArrayList<WeighedgraphEdge> edgesOf(int vertex) {
+        return adj.get(vertex);
+    }
+
+    /**
+     * @return list of all edges in the graph.
+     */
+    public ArrayList<WeighedgraphEdge> edges() {
+        ArrayList list = new ArrayList<WeighedgraphEdge>();
+
+        for (int from : adj.keySet()) {
+            ArrayList<WeighedgraphEdge> currentEdges = adj.get(from);
+            for (WeighedgraphEdge e : currentEdges) {
+                list.add(e);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * @return iterable of all vertices in the graph.
+     */
+    public Iterable<Integer> vertices() {
+        HashSet set = new HashSet();
+        for (WeighedgraphEdge edge : edges()) {
+            set.add((edge).from());
+            set.add(edge.to());
+        }
+
+        return set;
+    }
+
+    /**
+     * @return size of adjacency list
+     */
+    public int size() { 
+    	return adj.size();
+    	}
+
+    /**
+     * @return string representation of digraph
+     */
+    public String toString() {
+        String out = "";
+        for (int from : adj.keySet()) {
+            ArrayList<WeighedgraphEdge> currentEdges = adj.get(from);
+            out += from + " -> ";
+
+            if (currentEdges.size() == 0)
+                out += "-,";
+
+            for (WeighedgraphEdge edge : currentEdges)
+                out += edge.to() + " @ " + edge.weight() + ", ";
+
+            out += "\n";
+        }
+
+        return out;
+    }
+
+    /**
+     * Add new edge to the system.
+     * @param newEdge
+     */
+   // public void addEdge(WeighedgraphEdge newEdge) {
+        // create empty connection set
+     //   if (!adj.containsKey(newEdge.from()))
+       //     adj.put(newEdge.from(), new ArrayList<WeighedDigraphEdge>());
+
+        //ArrayList<WeighedDigraphEdge> currentEdges = adj.get(newEdge.from());
+
+        /* Check if edge already exists,
+         * if it is, replace it with new one assuming it needs to be updated */
+        //boolean edgeExists = false;
+        //for (int i = 0; i < currentEdges.size(); i++) {
+          //  if (currentEdges.get(i).to() == newEdge.to()) {
+            //    currentEdges.set(i, newEdge);
+              //  edgeExists = true;
+                //break;
+            //}
+        //}
+
+        //if (!edgeExists)
+          //  currentEdges.add(newEdge);
+
+        //adj.put(newEdge.from(), currentEdges);
+    //}
+
+    /**
+     * Graph Tests
+     * @param args
+     */
+   // public static void main(String args[]) {
+     //   WeighedDigraph graph = new WeighedDigraph();
+
+       // WeighedDigraphEdge edge1 = new WeighedDigraphEdge(1, 2, 2.0);
+        //WeighedDigraphEdge edge2 = new WeighedDigraphEdge(2, 1, 2.0);
+
+        //graph.addEdge(edge1);
+        //graph.addEdge(edge2);
+
+        //System.out.print(graph);
+    //}
+}
---
+++ WeighedgraphEdge.java
@@ -0,0 +1,32 @@
+package Project_2;
+public class WeighedgraphEdge {
+    private int from, to;
+    private double weight;
+
+    /**
+     * Construct graph edge
+     * @param from
+     * @param to
+     * @param weight
+     */
+    public WeighedgraphEdge(int from, int to, double weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    /**
+     * @return from vertex
+     */
+    public int from() { return from; }
+
+    /**
+     * @return to vertex
+     */
+    public int to() { return to; }
+
+    /**
+     * @return weight of edge between from() and to()
+     */
+    public double weight() { return weight; }
+}
